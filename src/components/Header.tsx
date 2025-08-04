8c0da545595b52cf4949a2264689894bad6bdcaf
username : kunalvishwakarmaa20@gmail.com <kunalvishwakarmaa20@gmail.com>
2025-08-05 01:13:19 +0530

    Initial commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -275,7 +275,7 @@ const Header = ({ extraClasses = "" }) => {
                         >
                           <div className="flex items-center gap-3">
                             <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                            <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                            <Moon className="h-5 w-5 absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                             Theme
                           </div>
                           <ChevronDown className="h-4 w-4" />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,24 +11,29 @@ import Marquee from "react-fast-marquee";
 export function Home() {
   return (
     <>
-      <Marquee className="w-full absolute top-14 bg-red-700 z-10 mt-4 ">
-        <h6 className="flex items-center gap-2">
+      <Header />
+      <Marquee
+        className="w-full bg-red-700 text-white z-10"
+        style={{ marginTop: "64px" }} 
+      >
+        <h6 className="flex items-center gap-2 p-2">
           <AlertTriangle className="w-5 h-5 " />
           Notice: If you can't see movies or get a blank screen, please change
           your DNS settings or use a VPN for full access.
         </h6>
       </Marquee>
-      {/* <QuickSearch /> */}
-      <Header extraClasses="fixed top-0 border-b-0 z-20 bg-background backdrop-blur-sm" />
-      <div className="grid md:min-h-screen h-[800px] w-full md:grid-cols-auto">
-        <HeroSection url={"/movie/upcoming"} />
-      </div>
-      {/* Popular movie list */}
-      <PopularMovies category={"now_playing"} />
-      <PopularMovies category={"upcoming"} />
-      <PopularMovies category={"popular"} />
-      <Tv />
-      <PopularMovies category={"top_rated"} />
+
+      <main>
+        <div className="w-full">
+          <HeroSection url={"/movie/upcoming"} />
+        </div>
+        {/* Popular movie list */}
+        <PopularMovies category={"now_playing"} />
+        <PopularMovies category={"upcoming"} />
+        <PopularMovies category={"popular"} />
+        <Tv />
+        <PopularMovies category={"top_rated"} />
+      </main>
       <Footer />
     </>
   );
